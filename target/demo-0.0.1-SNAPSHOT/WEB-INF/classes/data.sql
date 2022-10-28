db66137ee0b327634bf0a6588b950d7cb198f138
yunjinchoi <yunjinchoi89@gmail.com>
2022-10-28 16:13:04 +0900

    jar change

--- target/demo-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO menu VALUES(1, '아이스 아메리카노', 4500);
-
-INSERT INTO menu VALUES(2, '따뜻한 아메리카노', 4300);
--- target/demo-0.0.1-SNAPSHOT/WEB-INF/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-create table menu
-(
-   id integer not null,
-   menu_name varchar(255) not null,
-   price integer not null,
-   primary key(id)
-);
